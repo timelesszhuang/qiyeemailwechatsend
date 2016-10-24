18ae1c101d7a3d28be99088912779f1ca67602a2
timelessz <834916321@qq.com>
2016-10-24 09:43:12 +0800

    测试出错原因

--- application/index/controller/Sysevent.php
+++ application/index/controller/Sysevent.php
@@ -13,7 +13,7 @@ class Sysevent extends Controller
         $encodingAesKey = "uYQlKaDCeljc1AEDzIDNrFHGQF6haPh9Lsdv1InA9qA";
         //企业号后台随机填写的token
         $token = "eCL75fcA";
-        //引入放在Thinkphp下的Library/Vendor/wechat 下的微信加解密包
+        //引入放在Thinkphp下的wechat 下的微信加解密包
         Loader::import('wechat.WXBizMsgCrypt', EXTEND_PATH, '.php');
         //安装官方要求接收4个get参数 并urldecode处理
         // 获取当前请求的name变量
@@ -22,14 +22,18 @@ class Sysevent extends Controller
         $nonce = urldecode(Request::instance()->param('nonce'));
         $echostr = urldecode(Request::instance()->param('echostr'));
         //实例化加解密类
-        file_put_contents('$msg_signature:' . $msg_signature . '$timestamp:' . $timestamp . '$nonce' . $nonce . '$echostr:' . $echostr, 'a.txt', FILE_APPEND);
-        $wxcpt = new \WXBizMsgCrypt($token, $encodingAesKey, 'wxe041af5a55ce7365');
-        //解密数据最后 将解密后的数据返回给微信 成功会返回0 会将解密后的数据放入$echostr中
-        $errCode = $wxcpt->VerifyURL($msg_signature, $timestamp, $nonce, $echostr, $reecho);
-        if ($errCode == 0) {
-            echo $reecho;
-        } else {
-            print $errCode;
+        file_put_contents('a.txt', '$msg_signature:' . $msg_signature . '$timestamp:' . $timestamp . '$nonce:' . $nonce . '$echostr:' . $echostr, FILE_APPEND);
+        try {
+            $wxcpt = new \WXBizMsgCrypt($token, $encodingAesKey, 'wxe041af5a55ce7365');
+            //解密数据最后 将解密后的数据返回给微信 成功会返回0 会将解密后的数据放入$echostr中
+            $errCode = $wxcpt->VerifyURL($msg_signature, $timestamp, $nonce, $echostr, $reecho);
+            if ($errCode == 0) {
+                echo $reecho;
+            } else {
+                print $errCode;
+            }
+        } catch (Exception $e) {
+            file_put_contents('a.txt', $e->getMessage(), FILE_APPEND);
         }
     }
 }

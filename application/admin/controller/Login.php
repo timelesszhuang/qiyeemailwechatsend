3c622a8b117406943ffa0697d2486fc09b3cfac3
timelessz <834916321@qq.com>
2016-10-24 16:02:24 +0800

    新建应用

---
+++ application/admin/controller/Login.php
@@ -0,0 +1,21 @@
+<?php
+namespace app\index\controller;
+
+use think\Controller;
+use think\Loader;
+use think\Request;
+
+/**
+ *  不用账号登陆
+ */
+class Login extends Controller
+{
+    /**
+     * 接收系统事件
+     * @access public
+     */
+    public function index()
+    {
+        echo 'dsadas';
+    }
+}
---
+++ application/admin/controller/Usercallback.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\admin\controller;
+
+use think\Loader;
+use think\Request;
+
+class Usercallback
+{
+    public function index()
+    {
+    }
+}

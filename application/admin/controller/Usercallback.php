cf0c3054de001cae7821c98172d4f1ac9a9b8acb
timelessz <834916321@qq.com>
2016-10-24 16:03:44 +0800

    新建应用

--- application/admin/controller/Usercallback.php
+++ application/admin/controller/Usercallback.php
@@ -8,5 +8,32 @@ class Usercallback
 {
     public function index()
     {
+        //企业号后台随机填写的encodingAesKey
+        $encodingAesKey = "63zypb8isLdXy4hWEwYAhcqjBnoTYAt69YGD62VHzrY";
+        //企业号后台随机填写的token
+        $token = "xyLp3wkwVj8GomtUaIlqa";
+        //引入放在Thinkphp下的wechat 下的微信加解密包
+        Loader::import('wechat.WXBizMsgCrypt', EXTEND_PATH, '.php');
+        //安装官方要求接收4个get参数 并urldecode处理
+        // 获取当前请求的name变量
+        $msg_signature = urldecode(Request::instance()->param('msg_signature'));
+        $timestamp = urldecode(Request::instance()->param('timestamp'));
+        $nonce = urldecode(Request::instance()->param('nonce'));
+        $echostr = urldecode(Request::instance()->param('echostr'));
+        //实例化加解密类
+        //file_put_contents('a.txt', '$msg_signature:' . $msg_signature . '$timestamp:' . $timestamp . '$nonce:' . $nonce . '$echostr:' . $echostr, FILE_APPEND);
+        try {
+            $wxcpt = new \WXBizMsgCrypt($token, $encodingAesKey, 'wxe041af5a55ce7365');
+            //解密数据最后 将解密后的数据返回给微信 成功会返回0 会将解密后的数据放入$echos
+            $errCode = $wxcpt->VerifyURL($msg_signature, $timestamp, $nonce, $echostr, $reecho);
+            //file_put_contents('a.txt', 'errorcode:' . $errCode, FILE_APPEND);
+            if ($errCode == 0) {
+                echo $reecho;
+            } else {
+                print $errCode;
+            }
+        } catch (Exception $e) {
+            //file_put_contents('a.txt', '$exception:' . $e->getMessage(), FILE_APPEND);
+        }
     }
 }

e1b2c97a45f054f87f7d58ef2677e68ee21f133e
timelessz <834916321@qq.com>
2016-10-29 11:02:51 +0800

    修改数据库操作

--- application/admin/controller/Usercallback.php
+++ application/admin/controller/Usercallback.php
@@ -36,7 +36,7 @@ class Usercallback
                     //file_put_contents('a.txt', '$exception:' . $e->getMessage(), FILE_APPEND);
                 }*/
 
-	    file_put_contents('a.txt','1',FILE_APPEND);
+        file_put_contents('a.txt', '1', FILE_APPEND);
         $encodingAesKey = Config::get('wechatsuite.EMAILSEND_ENCODINGAESKEY');
         //企业号后台随机填写的token
         $token = Config::get('wechatsuite.EMAILSEND_TOKEN');
@@ -49,23 +49,23 @@ class Usercallback
         $msg_signature = urldecode(Request::instance()->param('msg_signature'));
         $timestamp = urldecode(Request::instance()->param('timestamp'));
         $nonce = urldecode(Request::instance()->param('nonce'));
-//        file_put_contents('auth.txt', '授权', FILE_APPEND);
+//      file_put_contents('auth.txt', '授权', FILE_APPEND);
         //实例化加解密类
         //授权的地方不是 使用suite_id 使用 try catch  一部分使用的是
         $sPostData = file_get_contents("php://input");
-        file_put_contents('a.txt','init_data'.$sPostData,FILE_APPEND);
-	$wxcpt = new \WXBizMsgCrypt($token, $encodingAesKey, $suite_id);
+        file_put_contents('a.txt', 'init_data' . $sPostData, FILE_APPEND);
+        $wxcpt = new \WXBizMsgCrypt($token, $encodingAesKey, $suite_id);
         $errCode = $wxcpt->DecryptMsg($msg_signature, $timestamp, $nonce, $sPostData, $sMsg);
         //验证通过
-        file_put_contents('a.txt','errorcode'.$errcode,FILE_APPEND);
-	if ($errCode == 0) {
+        file_put_contents('a.txt', 'errorcode' . $errCode, FILE_APPEND);
+        if ($errCode == 0) {
             $xml = new \DOMDocument();
             $xml->loadXML($sMsg);
             $reqToUserName = $xml->getElementsByTagName('ToUserName')->item(0)->nodeValue;
             $reqFromUserName = $xml->getElementsByTagName('FromUserName')->item(0)->nodeValue;
             $reqCreateTime = $xml->getElementsByTagName('CreateTime')->item(0)->nodeValue;
             $reqMsgType = $xml->getElementsByTagName('MsgType')->item(0)->nodeValue;
-            file_put_contents('a.txt', $reqToUserName.$reqFromUserName.$reqCreateTime, FILE_APPEND);
+            file_put_contents('a.txt', $reqToUserName . $reqFromUserName . $reqCreateTime, FILE_APPEND);
             //匹配类型
             switch ($reqMsgType) {
                 case "event":
@@ -79,38 +79,38 @@ class Usercallback
                     }
                     break;
                 //发送文本
-/*                case "text":
-                    $reqMsgContent = $xml->getElementsByTagName('Content')->item(0)->nodeValue;
-                    $user_str = $this->toUser();
-                    //获取发送者的salesman名称
-                    $send_salesman_name = $this->get_salesman_name();
-                    if (empty($send_salesman_name)) {
-                        return true;
-                    }
-                    $reqMsgContent = $send_salesman_name . ":" . $reqMsgContent;
-                    //如果要发送的用户不存在
-                    if (empty($user_str)) {
-                        return true;
-                    }
-                    $this->sendText($user_str, $reqMsgContent, C('AGENTID'));
-                    break;
-                case "image":
-                    $media_id = $xml->getElementsByTagName('MediaId')->item(0)->nodeValue;
-                    $user_str = $this->toUser();
+                /*                case "text":
+                                    $reqMsgContent = $xml->getElementsByTagName('Content')->item(0)->nodeValue;
+                                    $user_str = $this->toUser();
+                                    //获取发送者的salesman名称
+                                    $send_salesman_name = $this->get_salesman_name();
+                                    if (empty($send_salesman_name)) {
+                                        return true;
+                                    }
+                                    $reqMsgContent = $send_salesman_name . ":" . $reqMsgContent;
+                                    //如果要发送的用户不存在
+                                    if (empty($user_str)) {
+                                        return true;
+                                    }
+                                    $this->sendText($user_str, $reqMsgContent, C('AGENTID'));
+                                    break;
+                                case "image":
+                                    $media_id = $xml->getElementsByTagName('MediaId')->item(0)->nodeValue;
+                                    $user_str = $this->toUser();
 
-                    //获取发送者的salesman名称
-                    $send_salesman_name = $this->get_salesman_name();
-                    if (empty($send_salesman_name)) {
-                        return true;
-                    }
-                    //如果要发送的用户不存在
-                    if (empty($user_str)) {
-                        return true;
-                    }
-                    $send_text = $send_salesman_name . " 上传图片";
-                    $this->sendText($user_str, $send_text, C('AGENTID'));
-                    $this->sendImage($user_str, $media_id, C('AGENTID'));
-                    break;*/
+                                    //获取发送者的salesman名称
+                                    $send_salesman_name = $this->get_salesman_name();
+                                    if (empty($send_salesman_name)) {
+                                        return true;
+                                    }
+                                    //如果要发送的用户不存在
+                                    if (empty($user_str)) {
+                                        return true;
+                                    }
+                                    $send_text = $send_salesman_name . " 上传图片";
+                                    $this->sendText($user_str, $send_text, C('AGENTID'));
+                                    $this->sendImage($user_str, $media_id, C('AGENTID'));
+                                    break;*/
             }
         }
 

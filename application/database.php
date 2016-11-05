d7e620e019fce79f3bfe06d0776f635583afc751
timelessz <834916321@qq.com>
2016-11-05 15:50:27 +0800

    忽略提交

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type' => 'mysql',
     // 服务器地址
-    'hostname' => '',
+    'hostname' => '59.111.92.124',
     // 数据库名
-    'database' => '',
+    'database' => 'mailsend',
     // 用户名
     'username' => 'root',
     // 密码
-    'password' => '',
+    'password' => 'qiangbi123',
     // 端口
     'hostport' => '3306',
     // 连接dsn

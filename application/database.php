c422bccdf2c0d292e35e2b28c42d3086ac01ef47
timelessz <834916321@qq.com>
2016-11-06 12:29:28 +0800

    忽略提交

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type' => 'mysql',
     // 服务器地址
-    'hostname' => '59.111.92.124',
+    'hostname' => '59.111.92.203',
     // 数据库名
     'database' => 'mailsend',
     // 用户名

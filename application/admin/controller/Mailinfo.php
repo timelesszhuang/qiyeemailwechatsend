18d0df71cc63a91c4e9972674b7ca60478dd2e22
timelessz <834916321@qq.com>
2016-11-11 17:19:19 +0800

    每个企业的后台设置

---
+++ application/admin/controller/Mailinfo.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Home\Controller;
+
+use app\common\model\common;
+use think\Session;
+
+
+/**
+ * 邮箱账号信息
+ */
+class Mailinfo extends Base
+{
+
+    /**
+     * 获取邮箱信息
+     * @access public
+     */
+    public function get_domain_info()
+    {
+        //私钥
+        $prikey = Session::get('privatesecret');
+        //域名
+        $domain = Session::get('domain');
+        $product = Session::get('product');
+        $time = date(time()) . '000';
+        $res = openssl_pkey_get_private($prikey);
+        //需要逐条获取部门信息
+        //必须使用post方法
+        $src = "domain=" . $domain . "&product=" . $product . "&time=" . $time;
+        if (openssl_sign($src, $out, $res)) {
+            $sign = bin2hex($out);
+            $url = "https://apibj.qiye.163.com/qiyeservice/api/domain/getDomain";
+            $response_json = common::send_curl_request($url, $src . '&sign=' . $sign);
+            if ($response_json['suc']) {
+                $this->assign('record', $response_json['con']);
+            }
+        }
+        $this->display();
+    }
+
+}

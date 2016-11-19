dda49d8ee396be3c8fb356bfce774e53006dc2da
timelessz <834916321@qq.com>
2016-11-19 11:52:11 +0800

    每个企业的后台设置

--- application/index/model/SyseventModel.php
+++ application/index/model/SyseventModel.php
@@ -114,6 +114,7 @@ class SyseventModel
                     //永久授权码，并换取授权信息、企业access_token
                     $json_auth_info = common::send_curl_request($get_permanent_code_url, $post, 'post');
                     $auth_info = json_decode($json_auth_info, true);
+                    file_put_contents('a.txt', 'auth_info:' . $auth_info, FILE_APPEND);
                     if (!auth::analyse_init_corp_auth($auth_info)) {
                         return;
                     }

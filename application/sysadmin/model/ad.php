1117995c76cf6ef5b0cc549521ab1cbcac401c21
timelessz <834916321@qq.com>
2016-11-24 11:57:30 +0800

    每个企业的后台设置

--- application/sysadmin/model/ad.php
+++ application/sysadmin/model/ad.php
@@ -45,7 +45,7 @@ class ad
                 $v = $adds[$start];
                 $perarticle = [
                     'title' => $v['title'],
-                    'pic_url' => $v['pic_url'],
+                    'pic_url' => Config::get('DOMAIN') . $v['pic_url'],
                     'url' => self::get_ads_url($v['id'])
                 ];
                 $articles[] = $perarticle;
@@ -74,6 +74,5 @@ class ad
     {
         return Config::get('common.ADS_READ_URL') . "?id={$add_id}";
     }
-
 
 }

169f1f3b0245efae7483d032ef8170795eff84bc
timelessz <834916321@qq.com>
2016-11-24 15:47:00 +0800

    修改广告展现页面

--- application/wap/controller/Ad.php
+++ application/wap/controller/Ad.php
@@ -20,13 +20,17 @@ class Ad extends Controller
      * 点击阅读广告信息
      * @access public
      */
-    public function index()
+    public function read()
     {
         $id = Request::instance()->param('id');
+        $where = ['id' => $id];
+        $m = Db::name('ads');
+        $count = $m->where($where)->field('readcount')->find()['readcount'];
+        $m->where($where)->update(['readcount' => ++$count]);
         if (!$id) {
             exit('您的请求异常。');
         }
-        return $this->fetch('index');
+        return $this->fetch('index', ['r' => $m->where($where)->find()]);
     }
 
 }

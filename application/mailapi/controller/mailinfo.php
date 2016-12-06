a4105f5cf602e2b8f7b8bd5235aa7676073ce08f
timelessz <834916321@qq.com>
2016-12-06 18:17:03 +0800

    接口开通之后 api 状态更换失败，bug 修改

--- application/mailapi/controller/mailinfo.php
+++ application/mailapi/controller/mailinfo.php
@@ -3,6 +3,7 @@
 namespace app\mailapi\controller;
 
 use app\common\model\common;
+use think\image\Exception;
 
 /**
  * 邮箱账号信息
@@ -25,14 +26,17 @@ class mailinfo
         //需要逐条获取部门信息
         //必须使用post方法
         $src = "domain=" . $domain . "&product=" . $product . "&time=" . $time;
-        if (openssl_sign($src, $out, $res)) {
-            $sign = bin2hex($out);
-            $url = "https://apibj.qiye.163.com/qiyeservice/api/domain/getDomain";
-            $response_json = json_decode(common::send_curl_request($url, $src . '&sign=' . $sign), true);
-            file_put_contents('a.txt', print_r($response_json, true), FILE_APPEND);
-            if ($response_json['suc']) {
-                return [$response_json['con'], true];
+        try {
+            if (openssl_sign($src, $out, $res)) {
+                $sign = bin2hex($out);
+                $url = "https://apibj.qiye.163.com/qiyeservice/api/domain/getDomain";
+                $response_json = json_decode(common::send_curl_request($url, $src . '&sign=' . $sign), true);
+                if ($response_json['suc']) {
+                    return [$response_json['con'], true];
+                }
             }
+        } catch (Exception $ex) {
+            file_put_contents('a.txt', print_r($ex->getMessage(), true), FILE_APPEND);
         }
         return [[], false];
     }

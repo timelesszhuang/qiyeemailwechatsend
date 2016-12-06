9245dd29da6d50b205e502d583f93f618f11741e
timelessz <834916321@qq.com>
2016-12-06 21:51:22 +0800

    接口开通之后 api 状态更换失败，bug 修改

--- application/mailapi/controller/mailinfo.php
+++ application/mailapi/controller/mailinfo.php
@@ -23,13 +23,14 @@ class mailinfo
     {
         file_put_contents('a.txt', $prikey, FILE_APPEND);
         file_put_contents('a.txt', $domain, FILE_APPEND);
-        file_put_contents('a.txt', $product , FILE_APPEND);
+        file_put_contents('a.txt', $product, FILE_APPEND);
         $time = date(time()) . '000';
-        $res = openssl_pkey_get_private($prikey);
-        //需要逐条获取部门信息
-        //必须使用post方法
-        $src = "domain=" . $domain . "&product=" . $product . "&time=" . $time;
         try {
+            $res = openssl_pkey_get_private($prikey);
+            //需要逐条获取部门信息
+            //必须使用post方法
+            $src = "domain=" . $domain . "&product=" . $product . "&time=" . $time;
+
             if (openssl_sign($src, $out, $res)) {
                 $sign = bin2hex($out);
                 $url = "https://apibj.qiye.163.com/qiyeservice/api/domain/getDomain";

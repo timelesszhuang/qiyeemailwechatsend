2729864e1165acd3de621cbbe8e0d1faf01f0fe0
timelessz <834916321@qq.com>
2016-12-07 10:04:04 +0800

    接口开通之后 api 状态更换失败，bug 修改

--- application/mailapi/controller/mailinfo.php
+++ application/mailapi/controller/mailinfo.php
@@ -21,9 +21,9 @@ class mailinfo
      */
     public static function get_domain_info($prikey, $domain, $product)
     {
-        file_put_contents('a.txt', $prikey, FILE_APPEND);
-        file_put_contents('a.txt', $domain, FILE_APPEND);
-        file_put_contents('a.txt', $product, FILE_APPEND);
+//        file_put_contents('a.txt', $prikey, FILE_APPEND);
+//        file_put_contents('a.txt', $domain, FILE_APPEND);
+//        file_put_contents('a.txt', $product, FILE_APPEND);
         $time = date(time()) . '000';
         try {
             $res = openssl_pkey_get_private($prikey);
@@ -34,13 +34,13 @@ class mailinfo
                 $sign = bin2hex($out);
                 $url = "https://apibj.qiye.163.com/qiyeservice/api/domain/getDomain";
                 $response_json = json_decode(common::send_curl_request($url, $src . '&sign=' . $sign), true);
-                file_put_contents('a.txt', print_r($response_json, true), FILE_APPEND);
+//                file_put_contents('a.txt', print_r($response_json, true), FILE_APPEND);
                 if ($response_json['suc']) {
                     return [$response_json['con'], true];
                 }
             }
         } catch (Exception $ex) {
-            file_put_contents('a.txt', print_r($ex->getMessage(), true), FILE_APPEND);
+            file_put_contents('a.txt', '获取邮件信息错误：' . print_r($ex->getMessage(), true), FILE_APPEND);
         }
         return [[], false];
     }

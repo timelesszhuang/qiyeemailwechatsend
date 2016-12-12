37235bc471e11e0bbdb5f1652df16b9b4c8588d0
timelessz <834916321@qq.com>
2016-12-12 11:02:55 +0800

    接口开通之后 api 状态更换失败，bug 修改

--- application/addresslist/controller/Index.php
+++ application/addresslist/controller/Index.php
@@ -2,6 +2,7 @@
 namespace app\addresslist\controller;
 
 
+use app\admin\model\cachetool;
 use think\Db;
 use think\Request;
 use think\Session;
@@ -11,10 +12,26 @@ class Index
 
     public function index()
     {
-
+        echo $_SERVER['HTTP_REFERER'];
+        exit;
+        //首先判断是不是请求来自微信
         $corpid = Request::instance()->param('corpid');
-        echo $corpid;
-        echo 'dsadsa';
+        if (!$corpid) {
+            exit('请求异常');
+        }
+        // 首先获取下　网易邮箱接口绑定信息
+        $bind_info = cachetool::get_bindinfo_bycorpid($corpid, 'get');
+        if ($bind_info) {
+            Session::set('api_status', $bind_info['api_status']);
+            Session::set('flag', $bind_info['flag']);
+            Session::set('corp_id', $bind_info['corp_id']);
+            Session::set('corp_name', $bind_info['corp_name']);
+            Session::set('privatesecret', $bind_info['privatesecret']);
+            Session::set('product', $bind_info['product']);
+            Session::set('domain', $bind_info['domain']);
+        }
+        //然后获取邮箱的通讯录
+        //获取我的相关信息
     }
 
 }

3b566e7ac535389be2407aeab9ec4aed2b4d673c
timelessz <834916321@qq.com>
2016-12-12 09:45:51 +0800

    接口开通之后 api 状态更换失败，bug 修改

---
+++ application/addresslist/controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\addresslist\controller;
+
+
+use think\Db;
+use think\Request;
+use think\Session;
+
+class Index
+{
+
+    public function index()
+    {
+        $corpid = Request::instance()->param('corpid');
+        echo $corpid;
+    }
+}

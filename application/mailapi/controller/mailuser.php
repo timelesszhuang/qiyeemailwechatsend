b67e4bed0e7f457527066b82ce1d1e9a5497c03f
timelessz <834916321@qq.com>
2016-12-22 10:08:57 +0800

    ''

--- application/mailapi/controller/mailuser.php
+++ application/mailapi/controller/mailuser.php
@@ -105,6 +105,7 @@ class mailuser
     private static function update_user($response_json, $user_m, $unit_id, $unit_name, $corp_id, $corp_name, $corpid)
     {
         //有种情况是 比如职员比较多的情况下
+	file_put_contents('a.txt',print_r($response_json,true),FILE_APPEND);
         if ($response_json['suc']) {
             Db::startTrans();
             try {
@@ -122,10 +123,11 @@ class mailuser
                         'corp_id' => $corp_id,
                         'corp_name' => $corp_name,
                         'corpid' => $corpid,
-                        'create_time' => strtotime($v['create_time']),
+                        'create_time' => substr($v['create_time'],0,10),
                     );
                     $data[] = $perdata;
                 }
+		file_put_contents('a.txt',print_r($data,true),FILE_APPEND);
                 $user_m->insertAll($data);
                 Db::commit();
             } catch (Exception $ex) {

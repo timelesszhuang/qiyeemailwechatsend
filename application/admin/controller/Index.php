1b555d8bf630006231d22adaef3a004cb8418ee5
timelessz <834916321@qq.com>
2016-12-29 14:19:14 +0800

    获取客户管理员的联系方式

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -22,7 +22,12 @@ class Index extends Base
      */
     public function index()
     {
-        return $this->fetch('index', ['msg' => '登录成功。']);
+
+        if (Session::has('api_status')) {
+            return $this->fetch('index', ['msg' => '登录成功。']);
+        } else {
+            return $this->fetch('index/index', ['notbind_msg' => '贵公司网易企业邮箱接口暂时不可用，请拨打 4006360163 （网易企业服务） 联系我们，或通过 在线咨询 联系我们。']);
+        }
     }
 
 

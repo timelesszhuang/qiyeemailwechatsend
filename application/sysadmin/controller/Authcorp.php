2e2964d06829ac4b89219c01c4b53d11e7618ccd
timelessz <834916321@qq.com>
2017-03-02 12:02:05 +0800

    修改 审核通过之后可以 推送审核成功失败。

--- application/sysadmin/controller/Authcorp.php
+++ application/sysadmin/controller/Authcorp.php
@@ -215,7 +215,7 @@ class Authcorp extends Base
         $d['corp_id'] = Request::instance()->param('corp_id');
         $d['addresslist_show'] = Request::instance()->param('addresslist_show');
         if (!$d['product'] || !$d['domain'] || !$d['corp_name'] || !$d['user_num']) {
-            return json(\app\sysadmin\model\common::form_ajaxreturn_arr('保存失败', '每一项内容都不能为空。', self::error));
+            exit(json(\app\sysadmin\model\common::form_ajaxreturn_arr('保存失败', '每一项内容都不能为空。', self::error)));
         }
         return $d;
     }

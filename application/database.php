e1d2b5811b80d8ebaa180d61ad41241709424d97
赵兴壮 <834916321@qq.com>
2022-03-23 09:39:27 +0800

    Update database.php

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type' => 'mysql',
     // 服务器地址
-    'hostname' => 'rdsfjnifbfjnifbo.mysql.rds.aliyuncs.com',
+    'hostname' => '',
     // 数据库名
     'database' => 'mailsendbeta1',
     // 用户名
